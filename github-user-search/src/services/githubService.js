51809d7f18a17f92ad8193a9be1dada8a012b639
Keolos <abdullahmohammednaji@gmail.com>
2025-09-17 10:39:37 +0000

    edited package.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,17 @@
+import  Axios  from 'axios';
+
 const GITHUB_API_URL = 'https://api.github.com';
 
 // Function to fetch user data from GitHub API
 export const fetchUserData = async (username) => {
   try {
-    const response = await fetch(`${GITHUB_API_URL}/users/${username}`);
-    
-    if (!response.ok) {
+    const response = await Axios.get(`${GITHUB_API_URL}/users/${username}`);
+
+    if (response.status !== 200) {
       throw new Error('User not found');
     }
-    
-    const userData = await response.json();
-    return userData;
+
+    return response.data;
   } catch (error) {
     throw error;
   }
